464ea74c5afcc77aad150cbedaa6bd27ec964568
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 20:29:49 +0530

    [Sushant1]: Verify Pune

---
+++ src/Git/Pune_1.java
@@ -0,0 +1,19 @@
+package Git;
+
+public class Pune_1 
+{
+
+	static
+	{
+		System.out.println("My name is \"Hello all....\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+	
+	
+	
+}
