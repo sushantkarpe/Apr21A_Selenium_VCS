f4c381fd67a8c49b3cdefa42f479bd39e723aa2c
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 21:37:15 +0530

    [Sneha]: New Git Push

--- src/GitHub1/Sneha.java
+++ src/GitHub1/Sneha.java
@@ -5,12 +5,11 @@ public class Sneha
 
 	static
 	{
-		System.out.println("My name is \"Sneha1 is Active now..\"");
+		System.out.println("My name is \"Sneha1111 is Active now..\"");
 	}
 	
 	public static void main(String[] args)
 	{
-		
 		
 				
 	}
