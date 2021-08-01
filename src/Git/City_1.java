14bd32695b2fea89df65f7cd3c70e17d0e09334a
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 20:26:00 +0530

    [Sushant]: New Verification

---
+++ src/Git/City_1.java
@@ -0,0 +1,19 @@
+package Git;
+
+public class City_1 
+{
+
+	static
+	{
+		System.out.println("My name is \"Sushant is Active now..\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+	
+	
+	
+}
