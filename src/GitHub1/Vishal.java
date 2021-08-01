fe94047c399b7392bea5d45678d0c649879b941f
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 20:57:24 +0530

    [Vishal]: Push on Git..

---
+++ src/GitHub1/Vishal.java
@@ -0,0 +1,20 @@
+package GitHub1;
+
+public class Vishal 
+{
+
+	static
+	{
+		System.out.println("My name is \"Vishal is Active now..\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+				
+	}
+	
+	
+	
+	
+}
