869d5f335ef0b477e85ab65883574fc97715fa38
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 21:13:56 +0530

    [Urmila]: Push on GitHub

---
+++ src/GitHub1/Urmila.java
@@ -0,0 +1,18 @@
+package GitHub1;
+
+public class Urmila 
+{
+
+	static
+	{
+		System.out.println("My name is \"Urmila is Active now..\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+				
+	}
+	
+	
+}
