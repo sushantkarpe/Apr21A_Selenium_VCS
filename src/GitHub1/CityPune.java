c49ff0d0a58e00a88480e67f59b8a473daadb994
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 20:33:27 +0530

    [Sushant]: HSBC Company

---
+++ src/GitHub1/CityPune.java
@@ -0,0 +1,19 @@
+package GitHub1;
+
+public class CityPune 
+{
+
+	static
+	{
+		System.out.println("My name is \"HSBC Company\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+	
+	
+	
+}
