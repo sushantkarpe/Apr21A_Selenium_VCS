6e7651b3c482910d5897f214df61ad4bf939980b
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 20:37:58 +0530

    [Rahul]: Verify Rahul.

---
+++ src/GitHub1/Rahul.java
@@ -0,0 +1,20 @@
+package GitHub1;
+
+public class Rahul 
+{
+
+	static
+	{
+		System.out.println("My name is \"Rahul is Active now..\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		
+	}
+	
+	
+	
+	
+}
