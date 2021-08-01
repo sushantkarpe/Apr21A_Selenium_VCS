2bca8469d69503a9fa05dc2c35ff4af117906f3e
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 23:17:23 +0530

    [PravinD]: Push On Git

---
+++ src/GitHub1/PravinD.java
@@ -0,0 +1,16 @@
+package GitHub1;
+
+public class PravinD 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("hii..");
+		System.out.println("by..");
+		System.out.println("hii..");
+		System.out.println("by..");
+		System.out.println("hii..");
+	}
+	
+	
+}
