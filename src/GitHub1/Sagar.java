c340b246c4cd03f6acccbace887e95c588f84c32
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 21:05:12 +0530

    [Sagar]: Push on GitHub

---
+++ src/GitHub1/Sagar.java
@@ -0,0 +1,21 @@
+package GitHub1;
+
+public class Sagar 
+{
+
+	static
+	{
+		System.out.println("My name is \"Sagar is Active now..\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+				
+	}
+	
+	
+	
+	
+	
+}
