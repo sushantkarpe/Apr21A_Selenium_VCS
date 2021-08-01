12bb413d049f5ae288fdb31ed0d5bbb45c726b7d
Syali <Syali@LAPTOP-3QUIM19C>
2021-08-01 21:09:25 +0530

    [Sujay]: Push on Git

---
+++ src/GitHub1/Sujay.java
@@ -0,0 +1,19 @@
+package GitHub1;
+
+public class Sujay 
+{
+
+	static
+	{
+		System.out.println("My name is \"Sujay is Active now..\"");
+	}
+	
+	public static void main(String[] args)
+	{
+		
+				
+	}
+		
+	
+	
+}
